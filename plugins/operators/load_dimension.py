ecb3353a734aac2fc3efd3cea53dc5c5ac7fd693
mparrish44 <120215229+mparrish44@users.noreply.github.com>
2025-02-11 02:05:18 -0800

    Updated code project codes

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -8,15 +8,25 @@ class LoadDimensionOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
-                 *args, **kwargs):
+                 redshift_conn_id = "",
+                 sql= "",
+                 append=True,
+                 table="",
+                 *args,
+                 **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.sql = sql
+        self.append=append
+        self.table=table
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        self.log.info('LoadDimensionOperator', self.sql)
+        redshift_hook = PostgresHook(self.redshift_conn_id)
+
+        if self.table != "" and not self.append :
+            self.log.info("Clearing data from destination Redshift table")
+            redshift_hook.run("DELETE FROM {}".format(self.table))
+
+        redshift_hook.run(self.sql)
