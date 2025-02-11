284a8aea5465acf79012a884d35ac6f5fc52a92d
mparrish44 <120215229+mparrish44@users.noreply.github.com>
2025-02-11 03:45:42 -0800

    Updated code project codes

--- airflow1/plugins/operators/stage_redshift.py
+++ airflow1/plugins/operators/stage_redshift.py
@@ -69,21 +69,3 @@ class StageToRedshiftOperator(BaseOperator):
         formated_sql = formated_sql.replace("\n", "")
         self.log.info("debug sql run:", formated_sql)
         redshift.run(formated_sql)
-
-
-class StageToRedshiftOperator(BaseOperator):
-    ui_color = '#358140'
-
-    @apply_defaults
-    def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
-                 *args, **kwargs):
-        super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
-
-    def execute(self, context):
-        self.log.info('StageToRedshiftOperator not implemented yet')
