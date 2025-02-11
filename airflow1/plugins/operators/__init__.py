d0d72b03261795041c32725b0cef6fcccafb440a
mparrish44 <120215229+mparrish44@users.noreply.github.com>
2025-02-11 09:53:57 -0800

    Updated file

--- airflow1/plugins/operators/__init__.py
+++ airflow1/plugins/operators/__init__.py
@@ -1,19 +1,24 @@
 from __future__ import division, absolute_import, print_function
-from airflow.models import BaseOperator
 from airflow.plugins_manager import AirflowPlugin
-from operators.stage_redshift import StageToRedshiftOperator
 
-import operators
+# Import directly from their respective modules
+from operators.stage_redshift import StageToRedshiftOperator
+from operators.load_fact import LoadFactOperator
+from operators.load_dimension import LoadDimensionOperator
+from operators.data_quality import DataQualityOperator
+
+# Import helper module
 import helpers
+
 
 # Defining the plugin class
 class UdacityPlugin(AirflowPlugin):
     name = "udacity_plugin"
     operators = [
-        operators.StageToRedshiftOperator,
-        operators.LoadFactOperator,
-        operators.LoadDimensionOperator,
-        operators.DataQualityOperator
+        StageToRedshiftOperator,  # Use the direct class reference
+        LoadFactOperator,
+        LoadDimensionOperator,
+        DataQualityOperator
     ]
     helpers = [
         helpers.SqlQueries
