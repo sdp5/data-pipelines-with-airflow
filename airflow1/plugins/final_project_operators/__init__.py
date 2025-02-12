6d22427182bf15096a6f9b73f28bff07fc255425
mparrish44 <120215229+mparrish44@users.noreply.github.com>
2025-02-11 21:16:21 -0800

    Updated files

--- airflow1/plugins/final_project_operators/__init__.py
+++ airflow1/plugins/final_project_operators/__init__.py
@@ -2,7 +2,7 @@ from __future__ import division, absolute_import, print_function
 from airflow.plugins_manager import AirflowPlugin
 
 # Import directly from their respective modules
-from final_project_operators.stage_redshift import StageToRedshiftOperator
+from final_project_operators.stage_redshift import StageRedshiftOperator
 from final_project_operators.load_fact import LoadFactOperator
 from final_project_operators.load_dimension import LoadDimensionOperator
 from final_project_operators.data_quality import DataQualityOperator
